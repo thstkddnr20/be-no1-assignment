d03955ebcf65e39d103ed01dd5ce453af1f15bef
thstkddnr20 <thstkddnr20@naver.com>
2025-04-28 17:44:58 +0900

    lv2

---
+++ src/com/example/lv2/Calculator.java
@@ -0,0 +1,50 @@
+package com.example.lv2;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Calculator {
+    private List<Integer> resultData;
+
+    public Calculator() {
+        this.resultData = new ArrayList<>();
+    }
+
+    public List<Integer> getResultData() {
+        return resultData;
+    }
+
+    public void setResultData(List<Integer> resultData) {
+        this.resultData = resultData;
+    }
+
+    public void removeResult() {
+        resultData.remove(0);
+    }
+
+    public Integer calculate(int i1, int i2, char operator) {
+        Integer result = null;
+
+        if (operator == '+') {
+            result = i1 + i2;
+        } else if (operator == '-') {
+            result = i1 - i2;
+        } else if (operator == '*') {
+            result = i1 * i2;
+        } else if (operator == '/'){
+            if (i2 == 0) {
+                System.out.println("두번 째 정수가 0일 경우 나머지를 할 수 없습니다");
+            }
+            else {
+                result = i1 / i2;
+            }
+        } else {
+            System.out.println("올바른 연산 기호를 입력하세요");
+        }
+
+        if (result != null) {
+            resultData.add(result);
+        }
+        return result;
+    }
+}
---
+++ src/com/example/lv2/CalculatorMainV2.java
@@ -0,0 +1,50 @@
+package com.example.lv2;
+
+import java.util.Scanner;
+
+public class CalculatorMainV2 {
+    public static void main(String[] args) {
+        Calculator calculator = new Calculator();
+        Scanner sc = new Scanner(System.in);
+
+        while (true) {
+            System.out.print("첫 번째 숫자를 입력하세요: ");
+            int i1 = sc.nextInt();
+
+            System.out.print("두 번째 숫자를 입력하세요: ");
+            int i2 = sc.nextInt();
+
+            if (i1 < 0 || i2 < 0) {
+                System.out.println("양의 정수를 입력해주세요");
+                continue;
+            }
+
+            System.out.print("사칙연산 기호를 입력하세요: ");
+            char operator = sc.next().charAt(0);
+
+            Integer result = calculator.calculate(i1, i2, operator);
+
+            if (result != null) {
+                System.out.println("결과: " + result);
+            } else {
+                continue;
+            }
+
+            //리스트 맨 앞 요소 삭제 가능
+            //calculator.removeResult();
+
+            while (true) {
+                System.out.print("더 계산하시겠습니까? (exit을 입력하여 종료) : ");
+                String s = sc.next();
+
+                if (s.equals("exit")) {
+                    System.exit(1);
+                } else {
+                    break;
+                }
+            }
+
+        }
+    }
+
+}
